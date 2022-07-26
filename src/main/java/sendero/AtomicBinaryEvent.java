52e177365ffdbccb68fc4caea27c204f90455cb2
Juan Andrade <juanandrade_20@hotmail.com>
2022-07-26 15:05:32 -0500

    AtomicBinaryEvent.from(Switch). Transforms a non-concurrent switch to an atomicBinaryEvent. The object will possess overlap countermeasures, supported by AtomicBinaryEventConsumer's inner state.

--- src/main/java/sendero/AtomicBinaryEvent.java
+++ src/main/java/sendero/AtomicBinaryEvent.java
@@ -1,12 +1,36 @@
 package sendero;
 
 import sendero.switchers.Switchers;
-/**AtomicBinaryEvent is intended as a binary regressive event mediator between nodes within the reactive system,
- * a system which is inherently concurrent in nature.*/
+/**AtomicBinaryEvent is intended as a binary regressive event mediator between nodes within the reactive system, <p>
+ * a system inherently concurrent. <p>
+ * It adds 2 more states to the non-concurrent Switch: <p>
+ *         private static final int NOT_SET = -2, SHUT_DOWN = -1, ON = 1, OFF = 0; (see AtomicBinaryEventConsumer) <p>
+ * these states prevent: <p>
+ *  A) double overlapping signals between registration("start") && subsequent on(). <p>
+ *  B) double overlapping signals between off() & subsequent unregistration("shutdown") <p>
+ *
+ * Once a shutdown has been reached, no more signals will be received and a new allocation will be required.
+ * */
 public interface AtomicBinaryEvent extends Switchers.Switch {
     boolean start();
     boolean shutoff();
     boolean isOff();
+
+    /**Transforms a non-concurrent switch to an atomicBinaryEvent. <p>
+     * The object will possess overlap countermeasures, supported by AtomicBinaryEventConsumer's inner state. <p>
+     * The object won't have access to onStart() and onDestroyed() methods. <p>
+     * Once the event reaches a shutoff, no new events will be received. <p>
+     * If required, a new allocation for Switch must be created.
+     * */
+    static AtomicBinaryEvent from(Switchers.Switch aSwitch) {
+        return new AtomicBinaryEventConsumer() {
+            @Override
+            protected void onStateChange(boolean isActive) {
+                if (isActive) aSwitch.on();
+                else aSwitch.off();
+            }
+        };
+    }
 
     AtomicBinaryEvent DEFAULT = new AtomicBinaryEvent() {
         @Override
