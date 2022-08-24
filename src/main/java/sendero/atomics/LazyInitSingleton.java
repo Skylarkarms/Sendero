4e8c95d4afeca61612252371a4a4d85d3facb06a
Juan Andrade <juanandrade_20@hotmail.com>
2022-08-23 21:31:39 -0500

    LazyInitSingleton.java improved.

--- src/main/java/sendero/atomics/LazyInitSingleton.java
+++ src/main/java/sendero/atomics/LazyInitSingleton.java
@@ -6,30 +6,35 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 
 public class LazyInitSingleton<T> implements Supplier<T> {
-    private static final int NULL_PHASE = -1, CREATING_PHASE = 0, CREATED = 1; 
-    private final Pair.Immutables.Int<T> NULL = new Pair.Immutables.Int<>(NULL_PHASE, null),
-    CREATING = new Pair.Immutables.Int<>(CREATING_PHASE, null); 
-    private final AtomicReference<Pair.Immutables.Int<T>> ref = new AtomicReference<>(NULL);
-    private final Supplier<T> builder; 
+    private static final int NULL_PHASE = -1, CREATING_PHASE = 0, CREATED = 1;
+    private static final Pair.Immutables.Int<?> NULL = new Pair.Immutables.Int<>(NULL_PHASE, null),
+            CREATING = new Pair.Immutables.Int<>(CREATING_PHASE, null);
+    @SuppressWarnings("unchecked")
+    private static<T> T getNull() {
+        return (T) NULL;
+    }
+    @SuppressWarnings("unchecked")
+    private static<T> T getCreating() {
+        return (T) CREATING;
+    }
+    private final AtomicReference<Pair.Immutables.Int<T>> ref = new AtomicReference<>(getNull());
+    private final Supplier<T> builder;
     public LazyInitSingleton(Supplier<T> builder) {
-        this.builder = builder; 
-    } 
-    public T getAndDestroy() { 
-        return ref.getAndSet(NULL).value; 
-    } 
-    @Override 
-    public T get() { 
-        Pair.Immutables.Int<T> prev; 
-        do { 
-            if (ref.compareAndSet(NULL, CREATING)) { 
-                T res = builder.get(); 
-                ref.set(new Pair.Immutables.Int<>(CREATED, res)); 
-                return res; 
-            } else { 
-                prev = ref.get(); 
-                if (prev.anInt == CREATED) return prev.value;
-            } 
-        } while (prev.anInt < CREATED);
-        return prev.value; 
-    } 
+        this.builder = builder;
+    }
+    public T getAndDestroy() {
+        return ref.getAndSet(getNull()).value;
+    }
+    @Override
+    public T get() {
+        Pair.Immutables.Int<T> prev;
+        while ((prev = ref.get()).anInt < CREATED) {
+            if (ref.compareAndSet(getNull(), getCreating())) {
+                T res = builder.get();
+                ref.set(new Pair.Immutables.Int<>(CREATED, res));
+                return res;
+            }
+        }
+        return prev.value;
+    }
 }
