9f079c3aaf00ceef5e82040701faf33d75016586
Juan Andrade <juanandrade_20@hotmail.com>
2022-09-05 17:44:15 -0500

    Updater.java doc added.

--- src/main/java/sendero/interfaces/Updater.java
+++ src/main/java/sendero/interfaces/Updater.java
@@ -6,9 +6,12 @@ public interface Updater<T> {
     /**The
      * function should be side-effect-free, since it may be re-applied
      * when attempted updates fail due to contention among threads.
-     * @return: the updated value.
-     * null if expectIn returned false*/
+     * @return: the updated value. || null if Predicate test == false*/
     T updateAndGet(UnaryOperator<T> update);
+    /**The
+     * function should be side-effect-free, since it may be re-applied
+     * when attempted updates fail due to contention among threads.
+     * @return: the updated value. || null if Predicate test == false*/
     T getAndUpdate(UnaryOperator<T> update);
     void update(long delay, UnaryOperator<T> update);
 }
