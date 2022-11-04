615a6bed54236dc3ba879aaa2751bb77b0cc5c30
Juan Andrade <juanandrade_20@hotmail.com>
2022-11-04 08:39:49 -0500

    Register nullCheck static util.

--- src/main/java/sendero/interfaces/Register.java
+++ src/main/java/sendero/interfaces/Register.java
@@ -5,4 +5,10 @@ import java.util.function.Consumer;
 @FunctionalInterface
 public interface Register<V> {
     void register(Consumer<? super V> valueConsumer);
+
+    static<V> Register<V> nullCheck(V value) {
+        return valueConsumer -> {
+            if (value != null) valueConsumer.accept(value);
+        };
+    }
 }
