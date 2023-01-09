f0cf235e164e6ce76716fe0397df622559486d31
Juan Andrade <juanandrade_20@hotmail.com>
2023-01-09 18:04:07 -0500

    Updater.java doc added.

--- src/main/java/sendero/interfaces/Updater.java
+++ src/main/java/sendero/interfaces/Updater.java
@@ -2,6 +2,10 @@ package sendero.interfaces;
 
 import java.util.function.UnaryOperator;
 
+/**Any successful update will trigger a dispatch event to all observers.
+ * An update is considered unsuccessful If the same instance is returned,
+ * or If any of the Logic gates prevent its swap.
+ * By default, an update will not proceed if the same instance is returned*/
 public interface Updater<T> {
     /**The
      * function should be side-effect-free, since it may be re-applied
@@ -13,5 +17,6 @@ public interface Updater<T> {
      * when attempted updates fail due to contention among threads.
      * @return: the updated value. || null if Predicate test == false*/
     T getAndUpdate(UnaryOperator<T> update);
+
     void update(long delay, UnaryOperator<T> update);
 }
