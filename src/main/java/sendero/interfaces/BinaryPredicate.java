4ca6d031cbf38962a2c1bb9f88cfadd4c2578b16
Juan Andrade <juanandrade_20@hotmail.com>
2023-06-01 11:20:57 -0500

    BinaryPredicate#nonNullTest added: @return true if either of them is null, BUT not the other,      OR true if both are null      else proceeds to perform the test

--- src/main/java/sendero/interfaces/BinaryPredicate.java
+++ src/main/java/sendero/interfaces/BinaryPredicate.java
@@ -4,9 +4,24 @@ import sendero.functions.Functions;
 
 import java.util.function.BiPredicate;
 
+@FunctionalInterface
 public interface BinaryPredicate<T> extends BiPredicate<T, T> {
     @Override
     boolean test(T next, T prev);
+
+    /**@return true if either of them is null, BUT not the other, <p>
+     * OR true if both are null <p>
+     * else proceeds to perform the test*/
+    static<T> BinaryPredicate<T> nonNullTest(BinaryPredicate<T> test) {
+        return (next1, prev1) -> {
+            boolean prevNull = false;
+            if (next1 == null || (prevNull = prev1 == null)) {
+                return !prevNull && prev1 == null;
+            } else {
+                return test.test(next1, prev1);
+            }
+        };
+    }
 
     @Override
     default BinaryPredicate<T> negate() {
