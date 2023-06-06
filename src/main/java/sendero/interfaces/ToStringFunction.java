afc9ef079cefb8e5c719ae93741232fc5770a444
Juan Andrade <juanandrade_20@hotmail.com>
2023-06-06 11:10:39 -0500

    Interfaces added: ToStringFunction.FromInt and ToStringFunction.FromDouble.

--- src/main/java/sendero/interfaces/ToStringFunction.java
+++ src/main/java/sendero/interfaces/ToStringFunction.java
@@ -4,4 +4,22 @@ import java.util.function.Function;
 
 @FunctionalInterface
 public interface ToStringFunction<T> extends Function<T, String> {
+
+    @FunctionalInterface
+    interface FromInt {
+        String asString(int integer);
+
+        default ToStringFunction<Integer> as() {
+            return this::asString;
+        }
+    }
+
+    @FunctionalInterface
+    interface FromDouble {
+        String asString(double aDouble);
+        default ToStringFunction<Double> as() {
+            return this::asString;
+        }
+
+    }
 }
