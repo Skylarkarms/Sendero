5498ff6b08c23fcfd1264639919298f35a3a891b
Juan Andrade <juanandrade_20@hotmail.com>
2023-06-13 11:15:02 -0500

    ToBooleanFunction.java, FromInt interface added.

--- src/main/java/sendero/interfaces/ToBooleanFunction.java
+++ src/main/java/sendero/interfaces/ToBooleanFunction.java
@@ -3,4 +3,13 @@ package sendero.interfaces;
 @FunctionalInterface
 public interface ToBooleanFunction<T> {
     boolean asBoolean(T t);
+
+    @FunctionalInterface
+    interface FromInt {
+        boolean asBoolean(int anInt);
+
+        default ToBooleanFunction<Integer> toParent() {
+            return FromInt.this::asBoolean;
+        }
+    }
 }
